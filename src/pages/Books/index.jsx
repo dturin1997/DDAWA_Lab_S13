db1bff8aba77a7073b4edc18361dac44e8630f9b
Dario Turin <dturin1997@gmail.com>
2022-11-20 11:49:18 -0500

    Se borró código comentado

--- src/pages/Books/index.jsx
+++ src/pages/Books/index.jsx
@@ -1,48 +1,18 @@
 import { Card, CardMedia, Typography, CardContent, Container, Box, Button, TextField, Grid } from "@mui/material";
-import { useEffect, useState} from "react";
+import { useState} from "react";
 import { searchBooks } from "../../services";
 import { BookItem } from "../../components";
 import { Header, Footer } from "../../components"
 
 const Books = () => {
-    /*
-    const [books,setBooks] = useState([]);
-    const searchBooks = async (search) => {
-        const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${search}`);
-        const busqueda = await response.json();
-        console.log(busqueda);
-
-    }
-
-    const handleChange = (event) => {
-        console.log(event.target.valu)
-        //setBooks(event.target.value);
-        //searchBooks(event.target.value);
-    };
-
-    useEffect(() => {
-        // llamo a la funciona
-        searchBooks("Pokemon");
-    }, []);
-    */
+    
     const [ search, setSearch ] = useState("");
-    //const [ booksArray, setBooksArray] = useState([]);
+    
     const [books, setBooks] = useState([]);
-    /*
-    const handleSearch = async () => {
-        try{
-            const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${search}&maxResults=30`);
-            const data = await response.json()
-            console.log(data)
-        }catch{
-            console.log("Error ", error)
-        }
-    }
-    */
+    
     const handleSearch = async () => {
         const books = await searchBooks(search);
         console.log(books);
-        //setBooksArray(books.items);
         setBooks(books.items);
 
     }
@@ -88,37 +58,7 @@ const Books = () => {
                     
                 </Card>
             </Box>
-{/* 
-            <Box>
-                <Grid container spacing={3} mt={3}>
-                    {booksArray.length > 0 &&
-                    booksArray.map(({volumeInfo}, index) => (
-                        <Grid item xs={12} sm={4} key={index}>
-                            <Card 
-                                sx={{ backgroundColor: 'rgb(250, 194, 19)', padding: "25px"  }}
-                            >
-                                <CardMedia
-                                className="card-media"
-                                sx={{ borderRadius: '50%', backgroundColor: 'rgb(255, 250, 215)' }}
-                                component="img"
-                                width={300}
-                                height={250}
-                                image={volumeInfo?.imageLinks?.thumbnail}
-                                />
-                                <CardContent>
-                                <Typography variant="body1"
-                                sx={{ color:'red', fontWeight: 'bold' }}>{volumeInfo.title}</Typography>
-                                
-                                <Button
-                                    sx={{ color:'green', fontWeight: 'bold' }}
-                                >Ver detalle</Button>
-                                </CardContent>
-                            </Card>
-                        </Grid>
-                    ))}
-                </Grid>
-            </Box>
-*/}
+
         {books.length > 0 &&
             books.map((book, index) => <BookItem key={index} book={book} />)}
 
